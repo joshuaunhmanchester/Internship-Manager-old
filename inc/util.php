af1f604682c29c401ca6e3b16d35239d1e71529f
Joshua Anderson <jsm69@wildcats.unh.edu>
2013-05-22 21:08:40 -0400

    Commited new files

---
+++ inc/util.php
@@ -0,0 +1,21 @@
+<?php
+  /*
+  * File: util.php
+  * Authors: Joshua Anderson
+  * Created: 5/23/13
+  * Updated: 5/23
+  * Description: Utility functions used throughout the site
+  */
+
+  /*
+  * Opens the database connection.
+  * @return object $conn that has connection data
+  */
+
+  function connect()
+  {
+
+  }
+
+
+?>
